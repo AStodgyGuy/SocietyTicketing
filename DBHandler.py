91cbaab31b1f365f319ffdeb5339ef0d4ad81487
wizardjet <cx.jet97@yahoo.co.uk>
2019-09-26 01:02:01 +0100

    added event table

--- DBHandler.py
+++ DBHandler.py
@@ -6,6 +6,7 @@ class DBHandler:
     NAME_MAX_CHAR = 20
     EMAIL_MAX_CHAR = 15
     ID_MAX_CHAR = 10
+    EVENT_MAX_CHAR = 50
 
     def __init__(self):
         print("Welcome to the SMIG app v0.2")
@@ -29,9 +30,12 @@ class DBHandler:
         table_person = f"CREATE TABLE IF NOT EXISTS smig_person (first_name VARCHAR({self.NAME_MAX_CHAR}), last_name VARCHAR({self.NAME_MAX_CHAR}), email VARCHAR({self.EMAIL_MAX_CHAR}) PRIMARY KEY NOT NULL)"
         table_membership = f"CREATE TABLE IF NOT EXISTS smig_membership (person_email VARCHAR({self.EMAIL_MAX_CHAR}) PRIMARY KEY NOT NULL, hasPaid BOOLEAN, CONSTRAINT FOREIGN KEY (person_email) REFERENCES smig_person (email))"
         table_ID = f"CREATE TABLE IF NOT EXISTS smig_ID (person_email VARCHAR({self.EMAIL_MAX_CHAR}) PRIMARY KEY NOT NULL, `type` BOOLEAN, `number` VARCHAR({self.ID_MAX_CHAR}), CONSTRAINT FOREIGN KEY (person_email) REFERENCES smig_person (email))"
+        table_event = f"CREATE TABLE IF NOT EXISTS smig_event (id INT PRIMARY KEY AUTO_INCREMENT, `name` VARCHAR({self.EVENT_MAX_CHAR}), price_non_member DECIMAL(10,2), price_member DECIMAL(10,2), `date` DATE, `time` TIME, `location` VARCHAR({self.NAME_MAX_CHAR}))"
+        
         self.query(table_person)
         self.query(table_membership)
         self.query(table_ID)
+        self.query(table_event)
 
     def query(self, statement):
         try:
