138c659ab5fde4df7467d650d517b66244d1d2e0
wizardjet <cx.jet97@yahoo.co.uk>
2019-09-26 01:14:54 +0100

    database complete

--- DBHandler.py
+++ DBHandler.py
@@ -31,11 +31,14 @@ class DBHandler:
         table_membership = f"CREATE TABLE IF NOT EXISTS smig_membership (person_email VARCHAR({self.EMAIL_MAX_CHAR}) PRIMARY KEY NOT NULL, hasPaid BOOLEAN, CONSTRAINT FOREIGN KEY (person_email) REFERENCES smig_person (email))"
         table_ID = f"CREATE TABLE IF NOT EXISTS smig_ID (person_email VARCHAR({self.EMAIL_MAX_CHAR}) PRIMARY KEY NOT NULL, `type` BOOLEAN, `number` VARCHAR({self.ID_MAX_CHAR}), CONSTRAINT FOREIGN KEY (person_email) REFERENCES smig_person (email))"
         table_event = f"CREATE TABLE IF NOT EXISTS smig_event (id INT PRIMARY KEY AUTO_INCREMENT, `name` VARCHAR({self.EVENT_MAX_CHAR}), price_non_member DECIMAL(10,2), price_member DECIMAL(10,2), `date` DATE, `time` TIME, `location` VARCHAR({self.NAME_MAX_CHAR}))"
-        
+        table_event_attendee = f"CREATE TABLE IF NOT EXISTS smig_event_attendee (event_id INT, person_email VARCHAR({self.EMAIL_MAX_CHAR}), amount_paid DECIMAL(10,2), CONSTRAINT FOREIGN KEY (person_email) REFERENCES smig_person (email), CONSTRAINT FOREIGN KEY (event_id) REFERENCES smig_event (id))"
+        table_event_guest = f"CREATE TABLE IF NOT EXISTS smig_event_guest (event_id INT, guest_name VARCHAR({self.NAME_MAX_CHAR}), amount_paid DECIMAL(10,2), CONSTRAINT FOREIGN KEY (event_id) REFERENCES smig_event (id))"
         self.query(table_person)
         self.query(table_membership)
         self.query(table_ID)
         self.query(table_event)
+        self.query(table_event_attendee)
+        self.query(table_event_guest)
 
     def query(self, statement):
         try:
@@ -44,11 +47,11 @@ class DBHandler:
             print(e)
             return None
 
-    # create tables
-
     # populate tables
 
     # convert to CSV
+
+    # convert from CSV
 
     # get values from tables
 
